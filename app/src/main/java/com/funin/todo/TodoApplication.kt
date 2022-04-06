bc20939a4b0908ea5b27c29575715a166fb21e3a
Jaemin <once29@naver.com>
2022-04-06 15:46:27 +0900

    Add initializing logic in TodoApplication

--- app/src/main/java/com/funin/todo/TodoApplication.kt
+++ app/src/main/java/com/funin/todo/TodoApplication.kt
@@ -1,7 +1,30 @@
 package com.funin.todo
 
 import android.app.Application
+import com.facebook.stetho.Stetho
+import com.funin.base.utils.DimensionToPixels
+import com.funin.todo.data.TodoSharedPreferencesImpl
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TodoApplication : Application()
+class TodoApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initDimensionToPixels()
+        initSharedPreferences()
+        initStetho()
+    }
+
+    private fun initDimensionToPixels() {
+        DimensionToPixels.initialize(applicationContext)
+    }
+
+    private fun initSharedPreferences() {
+        TodoSharedPreferencesImpl.init(applicationContext)
+    }
+
+    private fun initStetho() {
+        Stetho.initializeWithDefaults(this)
+    }
+}
