4851ad0f746f9411fec9d172045f8015d4320456
Jaemin <once29@naver.com>
2022-04-06 15:44:51 +0900

    Create Me

---
+++ app/src/main/java/com/funin/todo/data/vo/Me.kt
@@ -0,0 +1,9 @@
+package com.funin.todo.data.vo
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "me")
+data class Me(@PrimaryKey @ColumnInfo(name = "user_id") @SerializedName("user_id") val userId: String)
