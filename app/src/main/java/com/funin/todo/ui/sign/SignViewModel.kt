4897541703d2d99bcaad00bc42124e5278581563
Jaemin.Park <once29@naver.com>
2022-04-27 00:03:38 +0900

    Implement signing up user api

--- app/src/main/java/com/funin/todo/ui/sign/SignViewModel.kt
+++ app/src/main/java/com/funin/todo/ui/sign/SignViewModel.kt
@@ -2,46 +2,70 @@ package com.funin.todo.ui.sign
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.funin.base.extensions.combine
 import com.funin.todo.data.repository.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SignViewModel @Inject constructor(private val authRepository: AuthRepository) : ViewModel() {
 
-    private val _emailField: MutableStateFlow<String?> = MutableStateFlow("")
-
-    private val _nicknameField: MutableStateFlow<String?> = MutableStateFlow("")
-
-    private val _passwordField: MutableStateFlow<String?> = MutableStateFlow("")
-
-    private val _rewritePasswordField: MutableStateFlow<String?> = MutableStateFlow("")
+    private val _signUpUser: MutableStateFlow<SignUpUser> = MutableStateFlow(SignUpUser())
 
     private val _isLoading: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val isLoading: StateFlow<Boolean> = _isLoading
 
-    val isSignUpButtonEnabled: StateFlow<Boolean> = _passwordField.combine(_rewritePasswordField)
-        .map { (password, rewirePassword) ->
-            !password.isNullOrBlank() && !rewirePassword.isNullOrBlank() && password == rewirePassword
-        }
+    private val _errorMessage: MutableStateFlow<String?> = MutableStateFlow("")
+    val errorMessage: SharedFlow<String?> = _errorMessage.shareIn(
+        scope = viewModelScope,
+        started = SharingStarted.Eagerly,
+        replay = 0
+    )
+
+    val isSignUpButtonEnabled: StateFlow<Boolean> = _signUpUser
+        .map { it.isValidated() }
         .distinctUntilChanged()
         .stateIn(viewModelScope, SharingStarted.Lazily, false)
 
     fun setEmailField(email: String?) {
-        _emailField.value = email
+        _signUpUser.value = _signUpUser.value.copy(email = email)
     }
 
     fun setNicknameField(nickname: String?) {
-        _nicknameField.value = nickname
+        _signUpUser.value = _signUpUser.value.copy(nickname = nickname)
     }
 
     fun setPasswordField(password: String?) {
-        _passwordField.value = password
+        _signUpUser.value = _signUpUser.value.copy(password = password)
     }
 
-    fun setRewritePasswordField(rewirePassword: String?) {
-        _rewritePasswordField.value = rewirePassword
+    fun setRewritePasswordField(rewritePassword: String?) {
+        _signUpUser.value = _signUpUser.value.copy(rewritePassword = rewritePassword)
+    }
+
+    fun signup() {
+        val signUpUser = _signUpUser.value
+        if (!signUpUser.isValidated()) {
+            _errorMessage.value = "회원가입 할 수 없습니다."
+        }
+        if (signUpUser.email == null || signUpUser.nickname == null || signUpUser.password == null) return
+        viewModelScope.launch(Dispatchers.IO) {
+            authRepository.signup(signUpUser.email, signUpUser.nickname, signUpUser.password)
+        }
+    }
+
+    data class SignUpUser(
+        val email: String? = null,
+        val nickname: String? = null,
+        val password: String? = null,
+        val rewritePassword: String? = null
+    ) {
+        fun isValidated(): Boolean {
+            return !email.isNullOrBlank() && !nickname.isNullOrBlank()
+                    && !password.isNullOrBlank() && !rewritePassword.isNullOrBlank()
+                    && password == rewritePassword
+        }
     }
 }
